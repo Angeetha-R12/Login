70b7314cb684236b6d9bf0c4443a15e850255faa
Angeetha-R12 <125265849+Angeetha-R12@users.noreply.github.com>
2024-04-03 11:14:42 +0530

    Add files via upload

---
+++ demo4/src/main/java/com/example/demo4/service/LoginService.java
@@ -0,0 +1,53 @@
+package com.example.demo4.service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo4.model.Login;
+import com.example.demo4.repository.LoginRepo;
+
+
+
+@Service
+public class LoginService {
+    @Autowired
+    LoginRepo mr;
+    public Login create (Login mm)
+    {
+        return mr.save(mm);
+    }
+    public List<Login> getAll()
+    {
+        return mr.findAll();
+    }
+    public Login getMe(String userName)
+    {
+        return mr.findById(userName).orElse(null);
+    }
+    public boolean updateDetails(String userName,Login mm)
+        {
+            if(this.getMe(userName)==null)
+            {
+                return false;
+            }
+            try{
+                mr.save(mm);
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+public boolean deleteLogin(String userName)
+        {
+            if(this.getMe(userName) == null)
+            {
+                return false;
+            }
+            mr.deleteById(userName);
+            return true;
+        }
+
+}
